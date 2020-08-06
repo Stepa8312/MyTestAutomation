31dc5d74049e4ffdaf0b16741f2e56a5af3a4824
Nikolay Stepanets <nik.stepanets@gmail.com>
2020-08-06 16:18:12 +0300

    First test

---
+++ MyTestAutomation/Tests/parentTest/ParentTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestAutomation.Tests.parentTest
+{
+    class ParentTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+
+        }
+
+        [Test]
+        public void LaunchApp()
+        {
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.google.com.ua/");
+
+        }
+
+        [TearDown]
+        public void ShutDown()
+        {
+            driver.Close();
+        }
+    }
+}
+
